cfaf9aed33f63283a42fe4d8028e970b44e25c9c
KirillAdILF <k.adamov@g.nsu.ru>
2026-01-15 18:50:52 +0700

    Small_changes

--- C1(ty, T).py
+++ C1(ty, T).py
@@ -1,9 +1,15 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.size'] = 16 # Общий размер для всех элементов
+plt.rcParams['figure.figsize'] = [6, 6] # Все новые графики будут 8x5 дюймов
+
+
 # c against ty for certain T
 ty = np.array([20, 17.5, 15, 12.5, 10, 7.5, 5, 2.5, 1.25, 1, 0.5, 0.25])
 
+ty_p = np.array([6, 9, 17, 30])
+C_p = np.array([22, 17, 15, 13])/100
 
 # T = 1 ms
 C1= np.array([0.090, 0.097, 0.104, 0.116, 0.132, 0.159, 0.217, 0.373, 0.537, 0.573, 0.637, 0.657])
@@ -18,15 +24,18 @@ C3 = np.array([0.099, 0.105, 0.113, 0.124, 0.140, 0.169, 0.226, 0.381, 0.537, 0.
 C4 = np.array([0.0366, 0.0385, 0.0444, 0.0478, 0.0491, 0.0559, 0.0779, 0.1317, 0.1838, 0.1956, 0.2159, 0.2220])
 
 plt.figure(1)
-plt.title("C againt ty for certain T")
-plt.xlabel("ty, [us]")
+#plt.title("C againt ty for certain T")
+plt.xlabel("τ, [мкс]")
 plt.ylabel("C")
 
-plt.plot(ty, C1, label="T = 1 ms")
-plt.plot(ty, C2, label="T = 5 ms")
-plt.plot(ty, C3, label="T = 10 ms")
-plt.plot(ty, C4, label="T = 120 ms")
 
+plt.plot(ty, C1, label="T = 1 мс", color="blue")
+plt.plot(ty, C2, label="T = 5 мс", color="orange")
+plt.plot(ty, C3, label="T = 10 мс", color ="green")
+plt.plot(ty, C4, label="T = 120 мс", color="red")
+
+plt.scatter(ty_p/2, C_p, color = "black")
+plt.plot(ty_p/2, C_p, color = "black", label ="эксперимент")
 
 
 
--- contrast_ty_exp_1.py
+++ contrast_ty_exp_1.py
@@ -5,9 +5,8 @@ ty = np.array([6, 9, 17, 30])
 C = np.array([22, 17, 15, 13])/100
 
 
-plt.figure(1)
-plt.title("C againt ty for certain T")
-plt.xlabel("ty, [us]")
+#plt.title("C againt ty for certain T")
+plt.xlabel("τ, [мкс]")
 plt.ylabel("C")
 plt.scatter(ty/2, C)
 plt.plot(ty/2, C)
